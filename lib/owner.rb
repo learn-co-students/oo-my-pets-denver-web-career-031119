3bc50b0ce175b910c0951275a1b8ce614de546aa
Zach Pardee <zachpardee@gmail.com>
2019-03-14 13:31:46 -0600

    first commit

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,25 @@
 class Owner
-  # code goes here
+  attr_accessor :pets
+
+  @@all = []
+
+  def initialize(pets)
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+
+  def buy_cat(name)
+    new_cat = Cat.new(name, mood)
+    @pets[1] << new_cat
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name, mood)
+    @pets[2] << new_dog
+  end
+
+  def buy_fish(name)
+    new_fish = Fish.new(name, mood)
+    @pets[0] << new_fish
+  end
+
 end
